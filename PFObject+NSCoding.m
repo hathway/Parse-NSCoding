688101fbd4c30f14dcf1e59b6acfc699e98876d5
DJ Tarazona <me@djtarazona.com>
2014-06-14 21:41:16 -0700

    Fix saving of unarchived PFObjects

--- PFObject+NSCoding.m
+++ PFObject+NSCoding.m
@@ -28,18 +28,18 @@
 	[encoder encodeObject:[self parseClassName] forKey:kPFObjectClassName];
 	[encoder encodeObject:[self allKeys] forKey:kPFObjectAllKeys];
 	[encoder encodeBool:[self isDataAvailable] forKey:kPFObjectIsDataAvailableKey];
-	
+
 	//Serialize Parse timestamps
 	[encoder encodeObject:self.createdAt forKey:kPFObjectCreatedAtKey];
 	[encoder encodeObject:self.updatedAt forKey:kPFObjectUpdatedAtKey];
-	
+
 	//Serialize all non-nil Parse properties
 	//[self allKeys] returns only the @dynamic properties that are not nil
 	for (NSString* key in [self allKeys]) {
 		id value = self[key];
 		[encoder encodeObject:value forKey:key];
 	}
-	
+
 	//Serialize all non-Parse properties
 	NSDictionary* nonParseProperties = [self nonDynamicProperties];
 	[self encodeProperties:nonParseProperties withCoder:encoder];
@@ -52,7 +52,7 @@
 	NSString* parseClassName = [aDecoder decodeObjectForKey:kPFObjectClassName];
 	NSArray* allKeys = [aDecoder decodeObjectForKey:kPFObjectAllKeys];
 	BOOL isDataAvailable = [aDecoder decodeBoolForKey:kPFObjectIsDataAvailableKey];
-	
+
 	if ([self isMemberOfClass:[PFObject class]]) {
 		//If this is a PFObject, recreate the object using the Parse class name and objectId
 		self = [PFObject objectWithoutDataWithClassName:parseClassName objectId:objectId];
@@ -61,19 +61,19 @@
 		//If this is a PFObject subclass, recreate the object using PFSubclassing
 		self = [[self class] objectWithoutDataWithObjectId:objectId];
 	}
-	
+
 	if (self) {
-		
+
 		//Deserialize Parse timestamps
 		self.createdAt = [aDecoder decodeObjectForKey:kPFObjectCreatedAtKey];
 		self.updatedAt = [aDecoder decodeObjectForKey:kPFObjectUpdatedAtKey];
-		
+
 		//Deserialize all non-nil Parse properties
 		for (NSString* key in allKeys) {
             id obj = [aDecoder decodeObjectForKey:key];
 			self[key] = obj;
 		}
-		
+
 		//Deserialize all nil Parse properties with NSNull
 		//This is to prevent an NSInternalConsistencyException when trying to access them in the future
 		//Loop through all dynamic properties that aren't in [self allKeys]
@@ -83,18 +83,20 @@
 				self[key] = [NSNull null];
 			}
 		}
-		
+
 		//Deserialize all non-Parse properties
 		NSDictionary* nonParseProperties = [self nonDynamicProperties];
 		[self decodeProperties:nonParseProperties withCoder:aDecoder];
     }
-	
+
 	//Mark PFObject as not dirty
 	[self->operationSetQueue removeAllObjects];
-	
+    [self->operationSetQueue addObject:[NSMutableDictionary new]];
+    [self setValue:self->estimatedData forKey:@"serverData"];
+
 	//Mark PFObject with same hasBeenFetched value as before encoding
 	[self setValue:@(isDataAvailable) forKey:kPFObjectIsDataAvailableKey];
-	
+
     return self;
 }
 
